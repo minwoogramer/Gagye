58f05f4e3f86363ac18383782d16a01d6a19d133
minwoo <100527953+minwoogramer@users.noreply.github.com>
2022-09-29 03:54:18 +0900

    Update GLTFMinting.js

--- utils/Minting/GLTFMinting.js
+++ utils/Minting/GLTFMinting.js
@@ -21,3 +21,19 @@ export const MintingStart = async ({ address }) => {
     }
   }
 };
+export const MintingStart2 = async ({ address }) => {
+  if (address != "") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(mintAddress, GLTFabi, signer);
+    try {
+      const tx = await contract.mint(BigNumber.from(1), {
+        value: ethers.utils.parseEther((0.5).toString()),
+      });
+      const res = await tx.wait();
+      console.log(res);
+    } catch (err) {
+      console.log("error", err);
+    }
+  }
+};
